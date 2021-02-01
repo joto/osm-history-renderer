836fd161f3346e6487c18a9d07186b702971be64
Jochen Topf <jochen@topf.org>
2021-02-01 20:31:30 +0100

    Boost shared_ptr conversion to bool is explicit

--- importer/entitytracker.hpp
+++ importer/entitytracker.hpp
@@ -63,21 +63,21 @@ public:
      * returns if the tracker currently tracks a previous entity
      */
     bool has_prev() {
-        return m_prev;
+        return static_cast<bool>(m_prev);
     }
 
     /**
      * returns if the tracker currently tracks a current entity
      */
     bool has_cur() {
-        return m_cur;
+        return static_cast<bool>(m_cur);
     }
 
     /**
      * returns if the tracker currently tracks a "next" entity
      */
     bool has_next() {
-        return m_next;
+        return static_cast<bool>(m_next);
     }
 
     /**
