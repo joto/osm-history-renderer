cf490fc87375973e655ec7a1a6f8b3172113f33b
Jochen Topf <jochen@topf.org>
2021-02-11 22:20:01 +0100

    Use Mercator projection function from libosmium
    Remove dependency on proj library

--- importer/project.hpp
+++ importer/project.hpp
@@ -1,48 +1,24 @@
 #ifndef IMPORTER_PROJECT_HPP
 #define IMPORTER_PROJECT_HPP
 
-#include <proj_api.h>
+#include <osmium/geom/mercator_projection.hpp>
 
 class Project {
-private:
-    projPJ pj_3857, pj_4326;
-
-    Project() {
-        if (!(pj_3857 = pj_init_plus("+init=epsg:3857"))) {
-            throw std::runtime_error{"can't initialize proj4 with 3857"};
-        }
-        if (!(pj_4326 = pj_init_plus("+init=epsg:4326"))) {
-            throw std::runtime_error{"can't initialize proj4 with 4326"};
-        }
-    }
-
-    virtual ~Project() {
-        pj_free(pj_3857);
-        pj_free(pj_4326);
-    }
-
-    static Project& instance() {
-        static Project the_instance;
-        return the_instance;
-    }
-
-    bool _toMercator(double *lon, double *lat) {
-        double inlon = *lon, inlat = *lat;
-        *lon *= DEG_TO_RAD;
-        *lat *= DEG_TO_RAD;
-
-        int r = pj_transform(pj_4326, pj_3857, 1, 1, lon, lat, NULL);
-        if (r != 0) {
-            std::cerr << "error transforming POINT(" << inlon << " " << inlat << ") from 4326 to 3857)" << std::endl;
-            *lon = *lat = 0;
+public:
+    static bool toMercator(double *lon, double *lat) noexcept {
+        if (*lon < -180.0 ||
+            *lon > 180.0 ||
+            *lat < -osmium::geom::MERCATOR_MAX_LAT ||
+            *lat > osmium::geom::MERCATOR_MAX_LAT) {
             return false;
         }
-        return true;
-    }
 
-public:
-    static bool toMercator(double *lon, double *lat) {
-        return Project::instance()._toMercator(lon, lat);
+        const osmium::geom::Coordinates c{*lon, *lat};
+        const auto projected = osmium::geom::lonlat_to_mercator(c);
+        *lon = projected.x;
+        *lat = projected.y;
+
+        return true;
     }
 };
 
